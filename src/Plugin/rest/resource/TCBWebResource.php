1b2485f66a3d5ed0acd250828e23a415a7880077
AntonioXIII <aj72325@gmail.com>
2019-07-28 20:44:27 -0600

    TCB Web Resource Class Stub
    Setting up the class stub for TCBWebResource per the spec in #10.

---
+++ src/Plugin/rest/resource/TCBWebResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\tcb_auth_server\Plugin\rest\resource;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Component\Serialization\Json;
+use Drupal\Component\Utility\Html;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class TCBWebResource extends ResourceBase {
+  
+  protected function getTermByID($tid, $fields) {
+    
+    
+    
+  }
+  
+  protected function getTermByName($name, $fields) {
+    
+    
+    
+  }
+  
+  private function getTermValue() {
+    
+    
+    
+  }
+  
+}
